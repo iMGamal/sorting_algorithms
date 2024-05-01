6abb2eda01f3029a7f36512b1ba89a0890eb347f
iMGamal <mohamed.gamal126027@gmail.com>
2024-05-01 12:18:48 +0000

    betty check

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,17 +5,17 @@
 /**
  * bubble_sort - Entry point
  * Description: function that sorts arrays
- * array: input to be sorted
- * size: size of input to be sorted
+ * array: array of integers to be sorted
+ * size: unsigned integer refers to input to be sorted
  * Return: Always 0 (Success)
  */
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -37,4 +37,16 @@ void bubble_sort(int *array, size_t size)
 			printf("%d", array[i]);
 		}
 	}
+}
+int main()
+{
+	int array[] = {17, 13, 15, 12, 11};
+
+	int n = sizeof(array)/sizeof(array[0]);
+
+	bubble_sort(array, n);
+
+	printf("\n");
+
+	return (0);
 }
