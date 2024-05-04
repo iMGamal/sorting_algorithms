c1144a1a8b6b676cb0df2f7bd253d5fd2c4f8be1
iMGamal <mohamed.gamal126027@gmail.com>
2024-05-04 21:52:08 +0000

    T

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j, k;
+	unsigned int i, j;
 
 	if (!array || !size)
 	{
@@ -29,7 +29,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 
 				array[j + 1] = key;
-				printarray(array, size);
+				print_array(array, size);
 			}
 		}
 		i++;
